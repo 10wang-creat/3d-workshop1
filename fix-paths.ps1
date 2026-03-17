f91c0328fa1fa85383ec834e0d6f7f13f7adbf87
10wang-creat <s881705@gmail.com>
2026-03-17 17:52:50 +0800

    更改圖片路徑

--- fix-paths.ps1
+++ fix-paths.ps1
@@ -1,3 +1,7 @@
-(Get-Content "C:\Users\s8817\OneDrive\Documents\GitHub\3d-workshop1\index.html" -Raw) -replace "'images/","'assets/images/" -replace '"images/','"assets/images/' | Set-Content "C:\Users\s8817\OneDrive\Documents\GitHub\3d-workshop1\index.html" -Encoding UTF8
-Remove-Item "C:\Users\s8817\OneDrive\Documents\GitHub\3d-workshop1\fix-paths.ps1"
-Write-Host "Done! All image paths updated to assets/images/"
+$file = "C:\Users\s8817\OneDrive\Documents\GitHub\3d-workshop1\index.html"
+$content = [System.IO.File]::ReadAllText($file, [System.Text.Encoding]::UTF8)
+$content = $content.Replace("'images/", "'assets/images/")
+$content = $content.Replace('"images/', '"assets/images/')
+[System.IO.File]::WriteAllText($file, $content, [System.Text.Encoding]::UTF8)
+$count = ([regex]::Matches($content, "assets/images/")).Count
+Write-Host "Replaced $count image paths to assets/images/"
