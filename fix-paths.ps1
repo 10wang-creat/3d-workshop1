8518a84a50a496cf390377b406bdea7d0c80b339
10wang-creat <s881705@gmail.com>
2026-06-17 22:08:56 +0800

    更新網頁

--- fix-paths.ps1
+++
@@ -1,7 +0,0 @@
-$file = "C:\Users\s8817\OneDrive\Documents\GitHub\3d-workshop1\index.html"
-$content = [System.IO.File]::ReadAllText($file, [System.Text.Encoding]::UTF8)
-$content = $content.Replace("'images/", "'assets/images/")
-$content = $content.Replace('"images/', '"assets/images/')
-[System.IO.File]::WriteAllText($file, $content, [System.Text.Encoding]::UTF8)
-$count = ([regex]::Matches($content, "assets/images/")).Count
-Write-Host "Replaced $count image paths to assets/images/"
